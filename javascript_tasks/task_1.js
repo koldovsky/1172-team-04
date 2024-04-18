7ad31871095718b8bcde3c94d6ef3ff3f22ea7ab
wicked-crystal <156825279+wicked-crystal@users.noreply.github.com>
2024-04-18 18:10:41 +0000

    add links to all tasks in task_1.js

--- javascript_tasks/task_1.js
+++ javascript_tasks/task_1.js
@@ -1,2 +1,25 @@
 // Task 1//
 // https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/javascript//
+
+// Task 2 Messi goals function//
+// https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
+
+
+// Task 3 Return Negative//
+//https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
+
+
+// Task 4 Grasshopper - Terminal game move function//
+//https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
+
+
+// Task 5 Grasshopper - Personalized Message//
+//https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript
+
+
+// Task 6 Keep Hydrated!
+//https://www.codewars.com/kata/keep-hydrated-1/train/javascript
+
+
+// Task 7 Opposites Attract
+//https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript
