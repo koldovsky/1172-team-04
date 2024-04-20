036bf5824ece319f918debec0051cae5131fae9e
SezamUA <135545759+SezamUA@users.noreply.github.com>
2024-04-20 22:55:15 +0300

    Update task_2.js

--- javascript_tasks/task_2.js
+++ javascript_tasks/task_2.js
@@ -1,10 +1,10 @@
 // Task 1 DNA to RNA Conversion
 // https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
-// Serhii Zamorii: https://www.codewars.com/kata/reviews/57e7742fe9068ed9f4000017/groups/57e82ddbf11c64130c000236
+// Serhii Zamorii: https://www.codewars.com/kata/reviews/55563a4e01231d19e00001e6/groups/639d7f42d379e90001a85e04
 
 // Task 2 Convert a string to an array
 // https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
-// Serhii Zamorii: https://www.codewars.com/kata/reviews/55563a4e01231d19e00001e6/groups/639d7f42d379e90001a85e04
+// Serhii Zamorii: https://www.codewars.com/kata/reviews/57e7742fe9068ed9f4000017/groups/57e82ddbf11c64130c000236
 
 // Task 3 Find Maximum and Minimum Values of a List
 // https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
