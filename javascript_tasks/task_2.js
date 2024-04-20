11263acc31a0cc6a2cd6741d455c62982b063f6a
SezamUA <135545759+SezamUA@users.noreply.github.com>
2024-04-20 21:38:43 +0300

    Update task_2.js

--- javascript_tasks/task_2.js
+++ javascript_tasks/task_2.js
@@ -1,14 +1,15 @@
 // Task 1 DNA to RNA Conversion
-//https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
-
+// https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
+// Serhii Zamorii: https://www.codewars.com/kata/reviews/57e7742fe9068ed9f4000017/groups/57e82ddbf11c64130c000236
 
 // Task 2 Convert a string to an array
-//https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
-
+// https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
+// Serhii Zamorii: https://www.codewars.com/kata/reviews/55563a4e01231d19e00001e6/groups/639d7f42d379e90001a85e04
 
 // Task 3 Find Maximum and Minimum Values of a List
 // https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
-
+// Serhii Zamorii: https://www.codewars.com/kata/reviews/577aec3cb9498e1aed00009a/groups/5ee6cb66e40f8f00013308c5
 
 // Task 4 Smallest value of an array
-//https://www.codewars.com/kata/544a54fd18b8e06d240005c0/train/javascript
+// https://www.codewars.com/kata/544a54fd18b8e06d240005c0/train/javascript
+// Serhii Zamorii: https://www.codewars.com/kata/reviews/544a5616dd73509376000092/groups/66240a6989a88b00016ada16
