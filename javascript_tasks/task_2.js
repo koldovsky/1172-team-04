0a25db024e26062ecfd8254f0a81605997c85054
wicked-crystal <156825279+wicked-crystal@users.noreply.github.com>
2024-04-21 18:37:03 +0000

    add my js solutions

--- javascript_tasks/task_2.js
+++ javascript_tasks/task_2.js
@@ -1,24 +1,29 @@
 // Task 1 DNA to RNA Conversion
 // https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
 // Serhii Zamorii: https://www.codewars.com/kata/reviews/55563a4e01231d19e00001e6/groups/639d7f42d379e90001a85e04
+// Khrystyna Vehera: https://www.codewars.com/kata/reviews/55563a4e01231d19e00001e6/groups/63a2f5d487d8f000017f59b6
 
 // Task 2 Convert a string to an array
 // https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
 // Serhii Zamorii: https://www.codewars.com/kata/reviews/57e7742fe9068ed9f4000017/groups/57e82ddbf11c64130c000236
+// Khrystyna Vehera: https://www.codewars.com/kata/reviews/57e7742fe9068ed9f4000017/groups/57e82ddbf11c64130c000236
 
 // Task 3 Find Maximum and Minimum Values of a List
 // https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
 // Serhii Zamorii: https://www.codewars.com/kata/reviews/577aec3cb9498e1aed00009a/groups/5ee6cb66e40f8f00013308c5
+// Khrystyna Vehera: https://www.codewars.com/kata/reviews/577aec3cb9498e1aed00009a/groups/577d8fbb0a8eb243da000084
 
 // Task 4 Smallest value of an array
 // https://www.codewars.com/kata/544a54fd18b8e06d240005c0/train/javascript
 // Serhii Zamorii: https://www.codewars.com/kata/reviews/544a5616dd73509376000092/groups/66240a6989a88b00016ada16
+// Khrystyna Vehera: https://www.codewars.com/kata/reviews/544a5616dd73509376000092/groups/5fe8c17b217ad5000161a38f
 
 // Additional optional tasks:
 
 // Task 5 A wolf in sheep's clothing
 // https://www.codewars.com/kata/a-wolf-in-sheeps-clothing/train/javascript
 // Serhii Zamorii: https://www.codewars.com/kata/reviews/5c8bfbbf3b8ec2000116c7cb/groups/5f95dab23677540001f0dfe2
+// Khrystyna Vehera: https://www.codewars.com/kata/reviews/5c8bfbbf3b8ec2000116c7cb/groups/6625585fc451bf000146bca6
 
 // Task 6 Beginner - Lost Without a Map
 // https://www.codewars.com/kata/beginner-lost-without-a-map
