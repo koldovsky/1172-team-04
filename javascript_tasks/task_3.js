ffd36693c3975c3fe2a65041fcb16c297c20f45b
SezamUA <135545759+SezamUA@users.noreply.github.com>
2024-04-23 22:04:25 +0300

    Create task_3.js

---
+++ javascript_tasks/task_3.js
@@ -0,0 +1,21 @@
+// Task 1 Find the smallest integer in the array
+// https://www.codewars.com/kata/find-the-smallest-integer-in-the-array/train/javascript
+// Serhii Zamorii: https://www.codewars.com/kata/reviews/55e81485eef300ee1c000186/groups/55f14905ef96f82d08000097
+
+// Task 2 Geometry Basics: Circle Circumference in 2D
+// https://www.codewars.com/kata/geometry-basics-circle-circumference-in-2d/train/javascript
+// Serhii Zamorii: https://www.codewars.com/kata/reviews/5b034f0113b4bfbcc9002e0a/groups/5b0358f613b4bfbb33002eae
+
+// Task 3 Training JS #12: loop statement --for..in and for..of
+// https://www.codewars.com/kata/training-js-number-12-loop-statement-for-dot-in-and-for-dot-of/train/javascript
+// Serhii Zamorii: https://www.codewars.com/kata/reviews/5722d8d3c341664bf800005d/groups/5caffa780839f30001773da1
+
+// Additional optional tasks:
+
+// Task 4 Understanding closures - the basics
+// https://www.codewars.com/kata/understanding-closures-the-basics/train/javascript
+// Serhii Zamorii: https://www.codewars.com/kata/reviews/56b721da70fb3ce639000046/groups/5c7f939fecdbb900014babe6
+
+// Task 5 Fun with ES6 Classes #2 - Animals and Inheritance
+// https://www.codewars.com/kata/fun-with-es6-classes-number-2-animals-and-inheritance/train/javascript
+// Serhii Zamorii: https://www.codewars.com/kata/reviews/56f953ba1c7b8a508100006e/groups/56fa5823e4d45d96b1001edd
