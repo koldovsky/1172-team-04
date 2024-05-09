31046634d4250d2cf6ba0c3d92baea8563ed4ad5
Nataliia <39300441+nataliiaiil@users.noreply.github.com>
2024-05-09 08:22:06 +0000

    added integration with products-service.js

--- js/index.best-deals.js
+++ js/index.best-deals.js
@@ -1,35 +1,8 @@
-import { addToCart } from "./common.cart-widget.js";
+import {Cart} from "./common.cart-widget.js";
+import {ProductsService} from "./products-service.js";
 
-const bestDealsWines = [
-  {
-    id: "1",
-    name: "Cabernet",
-    price: 17.0,
-    image: "images/index.best-deals/best-deals-cabernet.jpeg",
-    link: "cabernet",
-  },
-  {
-    id: "2",
-    name: "Sauvignon Blanc",
-    price: 12.0,
-    image: "images/index.best-deals/best-deals-sauvignon-blanc.jpeg",
-    link: "sauvignon-blanc",
-  },
-  {
-    id: "3",
-    name: "Syrah",
-    price: 13.0,
-    image: "images/index.best-deals/best-deals-syrah.jpeg",
-    link: "syrah",
-  },
-  {
-    id: "4",
-    name: "Pinot Grigio",
-    price: 10.0,
-    image: "images/index.best-deals/best-deals-pinot-grigio.jpeg",
-    link: "pinot-grigio",
-  },
-];
+const cart = new Cart();
+const bestDealsWines = await new ProductsService().getProducts();
 
 function getNextIdx(index) {
   return index === bestDealsWines.length - 1 ? 0 : index + 1;
@@ -42,16 +15,12 @@ function getPrevIdx(index) {
 function getBestDealsWineHTML(index) {
   const wine = bestDealsWines[index];
   const wineURL = `shop/${wine.link}`;
-  return `<div class="best-deals__item"><a href="${wineURL}"><img class="best-deals__item-picture" src="${
-    wine.image
-  }" alt="${wine.name}"></a>
-  <a href="${wineURL}" class="best-deals__item-name">${wine.name}</a>
-  <p class="best-deals__item-price">${wine.price
-    .toFixed(2)
-    .replace(".", ",")}USD</p>
-  <button class="best-deals__item-button best-deals__add-wine-${
-    wine.id
-  }">Add to cart</button></div>`;
+  return `<div class="best-deals__item">
+    <a href="${wineURL}"><img class="best-deals__item-picture" src="${wine.image}" alt="${wine.name}"></a>
+    <a href="${wineURL}" class="best-deals__item-name">${wine.name}</a>
+    <p class="best-deals__item-price">${wine.price.toFixed(2).replace(".", ",")}USD</p>
+    <button class="best-deals__item-button best-deals__add-wine-${wine.id}">Add to cart</button>
+  </div>`;
 }
 
 function renderBestDealsWines() {
@@ -117,18 +86,20 @@ function renderBestDealsWinePointers() {
 
 function addBestDealsWineButtonEvent(index) {
   const wine = bestDealsWines[index];
-  const button = document.querySelector(`.best-deals__add-wine-${wine.id}`);
-  button.addEventListener("click", () => {
-    addToCart(wine.id, 1);
-  });
+  document.querySelectorAll(`.best-deals__add-wine-${wine.id}`).forEach(
+    (el) =>
+      el.addEventListener("click", () => {
+        console.log(`Wine ${wine.id} added to cart`)
+        cart.addToCart(wine.id, 1);
+      }));
 }
 
 let currentWineIdx = 0;
 renderBestDealsWines();
 document
-  .querySelector(".best-deals__arrow-left")
-  .addEventListener("click", prevBestDealsWine);
+.querySelector(".best-deals__arrow-left")
+.addEventListener("click", prevBestDealsWine);
 document
-  .querySelector(".best-deals__arrow-right")
-  .addEventListener("click", nextBestDealsWine);
+.querySelector(".best-deals__arrow-right")
+.addEventListener("click", nextBestDealsWine);
 window.addEventListener("resize", renderBestDealsWines);
